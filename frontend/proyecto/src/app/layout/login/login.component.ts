31783b0eca439fed338d4a66be45d8b878edbce6
geraldineac <geraldineacevedo29@gmail.com>
2023-11-29 03:56:11 -0300

    mejorando login

--- frontend/proyecto/src/app/layout/login/login.component.ts
+++ frontend/proyecto/src/app/layout/login/login.component.ts
@@ -1,21 +1,59 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  emailValidator,
+  passValidator,
+  repeatPassValidator,
+  textValidator,
+} from 'src/app/customValidators/customValidators';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
- // password: string;
-  hide = true;
+  loginForm!: FormGroup;
+  showPass = false;
 
-  constructor(){}
+  constructor(private fb: FormBuilder) {}
 
-  passwordVisible() {
-    this.hide = !this.hide;
+  ngOnInit() {
+    this.loginForm = this.initLoginForm();
   }
 
-  login(){
-    console.log('Funcionando');
+  login() {
+    console.log(this.loginForm);
+  }
+
+  //Reutilizo codigo del register
+
+  /**
+   * Verifica si el campo tiene error despues de haber sido tocado.
+   *@param string:campo
+  */
+   invalidateField(field:string){
+    return ( this.loginForm.get(field)?.errors &&
+     this.loginForm.get(field)?.touched);
+  }
+
+  /**
+   * retorna el mensaje de error de la validacion.
+   *@param string:campo
+  */
+  validationFieldMessage(field:string):string{
+    return (this.loginForm.get(field)?.errors?.['message'])
+  }
+
+
+  /**
+   * crea un formulario para el inicio de sesión
+   * @returns retorna un formGroup
+   */
+  initLoginForm() {
+    return this.fb.group({
+      email: ['', [Validators.required, emailValidator]],
+      password: ['', [Validators.required, passValidator]],
+    });
   }
 }
--- frontend/proyecto/src/app/layout/login/login.module.ts
+++ frontend/proyecto/src/app/layout/login/login.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
@@ -12,7 +12,7 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     LoginRoutingModule
   ]
 })
