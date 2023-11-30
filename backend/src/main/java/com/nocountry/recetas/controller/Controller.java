b207ab515d6f40a6382f5c73c8aec1a0bdec26c7
Iran Mederos <iranmederos@gmail.com>
2023-11-29 21:59:13 -0300

    Endpoint para traer categorias

--- backend/src/main/java/com/nocountry/recetas/controller/Controller.java
+++ backend/src/main/java/com/nocountry/recetas/controller/Controller.java
@@ -1,4 +1,26 @@
 package com.nocountry.recetas.controller;
 
+import com.nocountry.recetas.domain.response.CategoriaResponse;
+import com.nocountry.recetas.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController("/")
 public class Controller {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping("categorias")
+    public ResponseEntity<List<CategoriaResponse>> getCategorias(){
+        List<CategoriaResponse> categoriaResponseList= categoriaService.getCategorias();
+        if(categoriaResponseList.isEmpty())
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(categoriaResponseList);
+    }
 }
