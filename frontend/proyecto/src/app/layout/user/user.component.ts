35be8616a33be0dbfb0d4272a7deca6f2d62a50d
geraldineac <geraldineacevedo29@gmail.com>
2023-11-30 22:12:17 -0300

    prueba

--- frontend/proyecto/src/app/layout/user/user.component.ts
+++ frontend/proyecto/src/app/layout/user/user.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
 })
 export class UserComponent {
+  username = 'Juanito'; //prueba de username
+  imageUrl = 'https://this-is-an-invalid-url.com/invalid-image.jpg'; //prueba de imagen que no carga
+  imageError = false;
 
+  onImageError() {
+    this.imageError = true;
+    console.log('La función onImageError se ha ejecutado.');
+  }
 }
