24736179e3ce1caaac283b2f9403859222a4cdd5
Dexametasona <machicadogomezalexander@gmail.com>
2023-12-02 16:31:22 -0500

    feat:add styles and structure to create-list view

--- frontend/proyecto/src/app/layout/create-list/create-list.module.ts
+++ frontend/proyecto/src/app/layout/create-list/create-list.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CreateListRoutingModule } from './create-list-routing.module';
 import { CreateListComponent } from './create-list.component';
+import { ListEditComponent } from './comps/list-edit/list-edit.component';
 
 
 @NgModule({
   declarations: [
-    CreateListComponent
+    CreateListComponent,
+    ListEditComponent
   ],
   imports: [
     CommonModule,
