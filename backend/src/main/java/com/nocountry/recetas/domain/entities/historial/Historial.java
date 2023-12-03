21748d22c42f770fa02c83197e0982a84f6fc722
RicardoTolomei <ricardoandrestolomei@gmail.com>
2023-12-03 16:53:56 -0300

    cambios
    cambios

--- backend/src/main/java/com/nocountry/recetas/domain/entities/historial/Historial.java
+++ backend/src/main/java/com/nocountry/recetas/domain/entities/historial/Historial.java
@@ -1,7 +1,18 @@
 package com.nocountry.recetas.domain.entities.historial;
-
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 public class Historial {
+
     @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
