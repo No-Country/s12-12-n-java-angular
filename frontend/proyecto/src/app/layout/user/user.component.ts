1b68aa4d61127a506a7fb1239a54d80bf43905d1
geraldineac <geraldineacevedo29@gmail.com>
2023-12-07 09:04:40 -0300

    quito codigo del ts de user que ya esta en ts de edit-profile

--- frontend/proyecto/src/app/layout/user/user.component.ts
+++ frontend/proyecto/src/app/layout/user/user.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { emailValidator, passValidator, repeatPassValidator, textValidator } from 'src/app/customValidators/customValidators';
-
 
 @Component({
   selector: 'app-user',
@@ -10,55 +6,5 @@ import { emailValidator, passValidator, repeatPassValidator, textValidator } fro
   styleUrls: ['./user.component.scss'],
 })
 export class UserComponent {
-  userForm!: FormGroup;
-  showPass = false;
-  showRepeatPass = false;
-
-  constructor(
-    private fb: FormBuilder,
-    private router:Router
-    ) {}
-
-  ngOnInit() {
-    this.userForm = this.initForm();
-  }
-
-  /**
-   * registra el nuevo usuario
-   */
-  onSubmit() {
-    console.log(this.userForm)
-
-  }
-
-  /**
-   * Verifica si el campo tiene error despues de haber sido tocado.
-   *@param string:campo
-  */
-  invalidateField(field:string){
-    return ( this.userForm.get(field)?.errors &&
-     this.userForm.get(field)?.touched);
-  }
-
-  /**
-   * retorna el mensaje de error de la validacion.
-   *@param string:campo
-  */
-  validationFieldMessage(field:string):string{
-    return (this.userForm.get(field)?.errors?.['message'])
-  }
-
-  /**
-   * crea un formulario para registrar usuarios
-   * @returns retorna un formGroup
-   */
-  initForm() {
-    return this.fb.group({
-      username: ['', [Validators.required, textValidator]],
-      password: ['', [Validators.required, passValidator]],
-      repeatPassword: ['',[Validators.required, repeatPassValidator, passValidator]],
-      email: ['',[ Validators.required, emailValidator]],
-    });
-  }
 
 }
