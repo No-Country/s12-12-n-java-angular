42d6f1908fd3f42523a18d6a760707ea7e60cd40
geraldineac <geraldineacevedo29@gmail.com>
2023-12-14 13:01:02 -0300

    soluciono login y register pero no estan responsivos

--- frontend/proyecto/src/app/components/navbar/navbar.component.ts
+++ frontend/proyecto/src/app/components/navbar/navbar.component.ts
@@ -1,28 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { navbarData } from '../../shared/navigation/nav-data';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  public showMenu: boolean = false;
+    public showMenu: boolean = false;
+    public scrolled: boolean = false;
+    navData = navbarData;
+    private router = inject(Router);
+    iconCookingWhite = '../../../assets/icons/logo.svg';
+    iconCookingBlack = '../../../assets/icons/logo-black.svg';
 
-  constructor(
-    private router:Router
-  ){}
+    @HostListener('window:scroll', ['$event'])
+    onScroll(): void {
+        this.scrolled = window.scrollY > 80 ? true : false;
+    }
+    /**
+     * The function returns a cooking icon based on whether the page has been scrolled or not.
+     * @returns a string value. The specific string value being returned depends on the value of the
+     * variable "scrolled". If "scrolled" is true, the function returns the value of the variable
+     * "iconCookingBlack". If "scrolled" is false, the function returns the value of the variable
+     * "iconCookingWhite".
+     */
+    get iconCooking(): string {
+        return this.scrolled ? this.iconCookingWhite : this.iconCookingBlack;
+    }
+    toggleMenu() {
+        console.log('toggleMenu abierto');
+        this.showMenu = !this.showMenu;
+    }
+    closeMenu() {
+        this.showMenu = false;
+    }
 
-  toggleMenu() {
-    console.log('toggleMenu abierto');
-    this.showMenu = !this.showMenu;
-  }
-  closeMenu() {
-    this.showMenu = false;
-  }
-
-  /*Metodo temporal para ver el usuario */
-  userRedirect(){
-    this.router.navigate(['user'])
-  }
+    /*Metodo temporal para ver el usuario */
+    userRedirect() {
+        this.router.navigate(['user'])
+    }
 }
---
+++ frontend/proyecto/src/app/shared/navigation/nav-data.ts
@@ -0,0 +1,18 @@
+export const navbarData = [
+  {
+      routerLink: '/home',
+      label: 'Inicio',
+  },
+  {
+      routerLink: '/categories',
+      label: 'Categorías',
+  },
+  {
+      routerLink: '/recipes',
+      label: 'Recetas',
+  },
+  {
+      routerLink: '/about',
+      label: 'Nosotros',
+  }
+];
