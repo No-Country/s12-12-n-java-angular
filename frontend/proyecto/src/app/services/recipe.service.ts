86e24d13f130e32a95f69ecd1d7d5571a20535e5
Raul Kevin Quispe diaz <raul.g.quispe@gmail.com>
2023-12-20 23:54:19 -0500

    feat:details recipe done,

--- frontend/proyecto/src/app/services/recipe.service.ts
+++ frontend/proyecto/src/app/services/recipe.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { IrecipeResponse } from '../interfaces/receta.interface';
 import { IcategoryRes } from '../interfaces/category.interface';
 import { IRepositoryRes } from '../interfaces/repository.interface';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,24 +13,29 @@ export class RecipeService {
   private readonly APIRECIPE = ENVIROMENT.apiRecipe;
   private readonly APICATEGORY = ENVIROMENT.apiCategory
   private readonly APIREPOSITORY = ENVIROMENT.apiRepository
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getAllRecipes(){
-    return this.http.get<IrecipeResponse[]>(this.APIRECIPE+"/get-recetas")
+  getAllRecipes() {
+    return this.http.get<IrecipeResponse[]>(this.APIRECIPE + "/get-recetas")
   }
 
-  getAllCategories(){
-    return this.http.get<IcategoryRes[]>(this.APICATEGORY+"/get-categorias")
+  getAllCategories() {
+    return this.http.get<IcategoryRes[]>(this.APICATEGORY + "/get-categorias")
   }
 
-  getAllRecipesById(){
-    return this.http.get<IRepositoryRes[]>(this.APIREPOSITORY+"/list")
+  getAllRecipesById() {
+    return this.http.get<IRepositoryRes[]>(this.APIREPOSITORY + "/list")
   }
 
-  deleteRecipeById(id:number){
-    return this.http.delete(this.APIREPOSITORY+"/delete?id="+id)
+  deleteRecipeById(id: number) {
+    return this.http.delete(this.APIREPOSITORY + "/delete?id=" + id)
   }
-  updateRecipeById(){
+  updateRecipeById() {
 
+  }
+  getRecipeById(id: number): Observable<IrecipeResponse | undefined> {
+    return this.http.get<IrecipeResponse>(`${this.APIRECIPE}/get-receta?id=${id}`).pipe(
+      catchError((err) => of(undefined))
+    )
   }
 }
